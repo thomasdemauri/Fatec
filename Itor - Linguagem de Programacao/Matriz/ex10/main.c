63dbc2c0ec5439bbf47e90afb5ceb4a537ec3c7b
Thomas Mauro <thom_mauro@outlook.com>
2024-10-24 10:26:27 -0300

    fixed

--- Itor - Linguagem de Programacao/Matriz/ex10/main.c
+++ Itor - Linguagem de Programacao/Matriz/ex10/main.c
@@ -14,10 +14,10 @@ char altera_estado(char estado);
 /**
  * A é número de andares do prédio
  * N é a quantidade de números, vamos chama-los de chaves
- * 
+ *
  * Cada andar tem sempre 4 apartamentos
  * Cada apartamento é (A * 10) + apartamento atual [0 - 1]
- * 
+ *
  * N[i] deve passar por todos apartamentos e comparar se apartamento % N[i] == 0
  * Se o apartamento for múltiplo, abrir a porta
  */
@@ -29,7 +29,7 @@ int main() {
     int n_chaves = 0;   // Número de chaves
 
     boot(&n_andares, &n_chaves);
-    
+
     int chaves[n_chaves];
     int predio[n_andares][APTOS];
     int portas[n_andares][APTOS];
@@ -45,11 +45,11 @@ int main() {
 
     return 0;
 }
-    
+
 char altera_estado(char estado) {
 
     if (estado == FECHADO) {
-        return ABERTO; 
+        return ABERTO;
     }
 
     return FECHADO;
@@ -60,7 +60,7 @@ void verifica_portas(int predio[][APTOS], int portas[][APTOS], int len, int *cha
     for (int numero=0; numero<len_chaves; numero++) {
         for (int andar=0; andar<len; andar++) {
             for (int apto=0; apto<APTOS; apto++) {
-                
+
                 if (predio[andar][apto] % chaves[numero] == 0) {
                     portas[andar][apto] = altera_estado(portas[andar][apto]);
                 }
